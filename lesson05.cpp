edd46c4bb05e20f28ba7c6ac35563d36efb6fe16
Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>
2024-11-11 08:54:55 +0100

    Add gradient_fast and gradient_fused_tilled

--- lesson05.cpp
+++ lesson05.cpp
@@ -1,6 +1,8 @@
 #include <Halide.h>
 #include <stdio.h>
 
+#include "halide_image_io.h"
+
 using namespace Halide;
 
 int main(int argc, char *argv[])
@@ -39,7 +41,7 @@ int main(int argc, char *argv[])
 		printf("Code for schedule:");
 		gradient.print_loop_nest();
 		printf("\n");
-		
+
 		printf("C code:\n");
 		for (int x = 0; x < N; x++) {
 			for (int y = 0; y < N; y++) {
@@ -52,20 +54,20 @@ int main(int argc, char *argv[])
 		Func gradient("gradient_split");
 		gradient(x,y) = x + y;
 		gradient.trace_stores();
-		
-		Var x_outer, x_inner;
-		gradient.split(x, x_outer, x_inner, N/2);
 
-		Buffer<int> out = gradient.realize({N, N});
+		Var x_outer, x_inner;
+		gradient.split(x, x_outer, x_inner, 2);
+
+		Buffer<int> out = gradient.realize({8, 3});
 
 		printf("Code for schedule:");
 		gradient.print_loop_nest();
 		printf("\n");
 
 		printf("C code:\n");
-		for (int y = 0; y < N; y++) {
-			for (int x_outer = 0; x_outer < N/2; x_outer++) {
-				for (int x_inner = 0; x_inner < N/2; x_inner++) {
+		for (int y = 0; y < 3; y++) {
+			for (int x_outer = 0; x_outer < 8/2; x_outer++) {
+				for (int x_inner = 0; x_inner < 2; x_inner++) {
 					int x = x_outer*2 + x_inner;
 					printf("Evaluate (%d, %d) = %d\n", x, y, x+y);
 				}
@@ -111,8 +113,8 @@ int main(int argc, char *argv[])
 
 		printf("C code:\n");
 
-		for (int y_outer = 0; y_outer < N; y_outer++) {
-			for (int x_outer = 0; x_outer < N; x_outer++) {
+		for (int y_outer = 0; y_outer < N/2; y_outer++) {
+			for (int x_outer = 0; x_outer < N/2; x_outer++) {
 				for (int y_inner = 0; y_inner < N; y_inner++) {
 					for (int x_inner = 0; x_inner < N; x_inner++) {
 						int x = x_outer*2 + x_inner;
@@ -125,7 +127,7 @@ int main(int argc, char *argv[])
 		printf("\n\n");
 	}
 
-	if (1) {
+	if (0) {
 		Func gradient("gradient_in_vectors");
 		gradient(x, y) = x + y;
 		gradient.trace_stores();
@@ -175,11 +177,76 @@ int main(int argc, char *argv[])
 
 		Var x_inner, x_outer;
 		gradient.split(x, x_outer, x_inner, 3);
-		gradient.unroll(x_inner);
 
 		Buffer<int> out = gradient.realize({7, 2});
 		printf("Code for schedule:");
 		gradient.print_loop_nest();
 		printf("\n");
+
+		printf("C code:\n");
+		for (int y = 0; y < 2; y++) {
+			int x_outer_factor =  7/3 + (7%3 != 0 ? 1 : 0);
+			for (int x_outer = 0; x_outer < x_outer_factor; x_outer++) {
+				for (int x_inner = 0; x_inner < 3; x_inner++) {
+					int x = x_outer*x_outer_factor;
+					if (x > 7 - 3)
+					 	x = 7 - 3;
+					x += x_inner;
+					printf("Evaluate (%d, %d) = %d\n", x, y, x+y);
+				}
+			}
+		}
+		printf("\n\n");
+	}
+	if (0) {
+		Func gradient("gradient_fused_tilled");
+		gradient(x, y) = x + y;
+		gradient.trace_stores();
+
+		Var x_inner, x_outer, y_inner, y_outer, tile_index;
+		gradient.split(x, x_outer, x_inner, 4);
+		gradient.split(y, y_outer, y_inner, 4);
+		gradient.reorder(x_inner, y_inner, x_outer, y_outer);
+		gradient.fuse(x_outer, y_outer, tile_index);
+		gradient.parallel(tile_index);
+
+		Buffer<int> out = gradient.realize({8, 8});
+		printf("Code for schedule:");
+		gradient.print_loop_nest();
+		printf("\n");
+	}
+
+	if (1) {
+		Func g("gradient fast");
+		g(x,y) = x+y;
+		g.trace_stores();
+
+		Var x_inner, x_outer, y_inner, y_outer, tile_index;
+		g
+			.tile(x, y, x_outer, y_outer, x_inner, y_inner, 64, 64)
+			.fuse(x_outer, y_outer, tile_index)
+			.parallel(tile_index);
+
+		Var x_inner_outer, y_inner_outer, x_vectors, y_pairs;
+		g
+			.tile(x_inner, y_inner, x_inner_outer, y_inner_outer, x_vectors, y_pairs, 4, 2)
+			.vectorize(x_vectors)
+			.unroll(y_pairs);
+
+		Buffer<int> out = g.realize({350, 250});
+		printf("Code for schedule:");
+		g.print_loop_nest();
+		printf("\n");
+
+		constexpr float max_val = 350.0f + 250.0f;
+		constexpr float scale_factor = 255.0 / max_val;
+		Func scale;
+		Expr val = out(x, y);
+
+		scale(x,y) = cast<uint8_t>(min(val * scale_factor, 255));
+		Buffer<uint8_t> im_out = scale.realize({out.width(), out.height()});
+
+		Tools::save_image(im_out, "gradient.png");
+
 	}
 }
