70a4a1a728bfb87b4d852a074acb667eff6dcb97
Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>
2025-01-13 11:06:43 +0100

    Update lesson13 with tuple reduction example

--- lesson13.cpp
+++ lesson13.cpp
@@ -49,5 +49,55 @@ int main(int argc, char *argv[])
 	Func multi_func2;
 	multi_func2(x, y) = { x + y, sin(x * y) };
 	multi_func2.realize({ 5, 10 });
+	Expr int_part = multi_func2(x, y)[0];
+	Expr flt_part = multi_func2(x, y)[1];
+
+	Func consumer;
+	consumer(x, y) = { int_part + 10, flt_part + 10.0f };
+
+	// Tuple reductions.
+	{
+		Func input_func;
+		input_func(x) = sin(x);
+		Buffer<float> input = input_func.realize({ 100 });
+
+		Func arg_max;
+		arg_max() = { 0, input(0) };
+
+		RDom r(1, 99);
+		Expr old_index = arg_max()[0];
+		Expr old_max = arg_max()[1];
+		Expr new_index = select(old_max < input(r), r, old_index);
+		Expr new_max = max(input(r), old_max);
+		arg_max() = { new_index, new_max };
+
+		Realization res = arg_max.realize();
+		Buffer<int> r0 = res[0];
+		Buffer<float> r1 = res[1];
+
+		printf("r0 %d r1 %f\n", r0(0), r1(0));
+
+		// for (int i = 0; i < 99; i++) {
+		// 	printf("%d %f\n", i, input(i));
+		// }
+		// The equivalent C++ is:
+		int arg_max_0 = 0;
+		float arg_max_1 = input(0);
+		for (int r = 1; r < 100; r++) {
+			int old_index = arg_max_0;
+			float old_max = arg_max_1;
+			int new_index = old_max < input(r) ? r : old_index;
+			float new_max = std::max(input(r), old_max);
+			// In a tuple update definition, all loads and computation
+			// are done before any stores, so that all Tuple elements
+			// are updated atomically with respect to recursive calls
+			// to the same Func.
+			arg_max_0 = new_index;
+			arg_max_1 = new_max;
+		}
+		assert(arg_max_0 == r0(0));
+		assert(arg_max_1 == r1(0));
+	}
+
 	printf("Done\n");
 }
